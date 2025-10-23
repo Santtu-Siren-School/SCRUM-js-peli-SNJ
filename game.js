0495a5cf4b7e291dd0d671d72e7f773f50b25ed0
Niilohyria <niilo.mustonen@edu.hyria.fi>
2025-10-23 20:04:27 +0300

    .

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 200 },
             debug: false
         }
     },
