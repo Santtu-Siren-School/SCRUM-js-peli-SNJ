a746377d8d03d97dd1a69a53a7f5e55b34369c01
Niilohyria <niilo.mustonen@edu.hyria.fi>
2025-10-23 12:42:03 +0300

    Lisäsin platformin

--- game.js
+++ game.js
@@ -10,15 +10,18 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-
+var platforms;
 function preload ()
 {
     this.load.image('background', 'background.png');
     this.load.spritesheet('main_character','tikku_hahmo.png',{frameWidth: 32, frameHeight: 48});
+    this.load.image('platform', 'assets/Platformit.png');
 }
 function create ()
 {
     this.add.image(400,300, 'background').setScale(4);
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 1000, 'Platformit').setScale(2).refreshBody();
 }
 
 function update ()
