49f9d8ff9ce263c368b4c01724b8c0adefa54eb7
Santtu-Siren-School <santtu.siren@edu.hyria.fi>
2025-10-23 11:26:56 +0300

    Lisäsin basic scenen, vain musta tällä hetkellä

---
+++ game.js
@@ -0,0 +1,25 @@
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.setBaseURL('https://labs.phaser.io');
+    }
+    create ()
+    {
+
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    }
+};
+const game = new Phaser.Game(config);
