18eff6a53a8340e4e3771c574008ec82e9d8fb91
Santtu-Siren-School <santtu.siren@edu.hyria.fi>
2025-12-08 09:35:07 +0200

    poistetiin 4th wall break

--- game.js
+++ game.js
@@ -2484,7 +2484,7 @@ class Level5 extends Phaser.Scene {
                         //katsotaan jos on phase 1
                         if(phase===1){
                             //anetaan random value 0-400, jos on 6 tee hyökäys ja pelaa animaatio
-                            bossattackchanche=Phaser.Math.Between(0, 150);
+                            bossattackchanche=Phaser.Math.Between(0, 200);
                             //console.log("boss attack chanche",bossattackchanche)
                             if (bossattackchanche===6) {
                                 boss_animation_play=true
@@ -2524,7 +2524,7 @@ class Level5 extends Phaser.Scene {
                         }
                         //sama kuin phase 1 mutta phase 2, pienemät arvot enemän hyökäyksiä (ja voimakaampia)
                         else if (phase===2){
-                            bossattackchanche=Phaser.Math.Between(0, 125);
+                            bossattackchanche=Phaser.Math.Between(0, 150);
                             //console.log("boss attack chanche",bossattackchanche)
                             if (bossattackchanche===6) {
                                 boss_animation_play=true
@@ -2905,7 +2905,6 @@ class end1 extends Phaser.Scene {
                                                             let end1img5 = this.add.image(500,450,'end1_5');
                                                             setTimeout(() => {
                                                                 end1img5.destroy();
-                                                                try_again.play();
                                                                 let end1img6 = this.add.image(550,480,'end1_6');
                                                                 setTimeout(() => {end1img6.destroy();this.scene.start('credit_scene')}, 7000);
                                                             }, cutscene_1);
@@ -3215,7 +3214,6 @@ const end3_player = new Audio('assets/sound/dialogue/uno_reverse_card.wav')
 const meeting_boss = new Audio('assets/sound/dialogue/arrogant_brat.wav')
 const voi_vittu = new Audio('assets/sound/dialogue/impending_doom.wav')
 const epic_fail = new Audio('assets/sound/dialogue/stupid_scream.wav')
-const try_again = new Audio('assets/sound/dialogue/death_doesnt_work_like_that.wav')
 const arrest = new Audio('assets/sound/dialogue/wannabe_cop.wav')
 const aww_you_want_yo_momma = new Audio('assets/sound/dialogue/rise_of_the_new_king.wav')
 const end2_background_song = new Audio('assets/sound/end2_background_song.mp3')
