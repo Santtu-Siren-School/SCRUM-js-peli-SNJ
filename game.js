e69b9156c644db6979751d3a69156717efcd3941
Santtu-Siren-School <santtu.siren@edu.hyria.fi>
2026-01-12 11:35:40 +0200

    poistetiin oikea kanuuna

--- game.js
+++ game.js
@@ -1078,7 +1078,10 @@ class Level2 extends Phaser.Scene {
         bottom_of_game.create(1500,900, 'bottom_of_game')
         bottom_of_game.create(1700,900, 'bottom_of_game')
         bottom_of_game.create(1900,900, 'bottom_of_game')
-        bottom_of_game.create(700,530, 'bottom_of_game').setScale(0.10).refreshBody();
+        bottom_of_game.create(700,515, 'bottom_of_game')
+        bottom_of_game.create(1100,515, 'bottom_of_game')
+        bottom_of_game.create(300,515, 'bottom_of_game')
+        bottom_of_game.create(100,515, 'bottom_of_game')
         //level2 bottom_of_game tekeminen
         //määritelään ovi
         ovi=this.physics.add.staticGroup();
@@ -1242,15 +1245,16 @@ this.time.delayedCall(1, () => enemy.wasHit = false);
     this.spikes.create(1525, 867, 'spike').setScale(0.8).refreshBody();
     this.spikes.create(1855, 867, 'spike').setScale(0.8).refreshBody();
     this.spikes.create(55, 877, 'spike').setScale(0.3).refreshBody();
-    this.spikes.create(205, 470, 'spike').setScale(3).refreshBody();
-    this.spikes.create(620, 510, 'spike').setScale(0.5).refreshBody();
-    this.spikes.create(775, 510, 'spike').setScale(0.5).refreshBody();
-    this.spikes.create(1090, 500, 'spike').setScale(1.5).refreshBody();
+    this.spikes.create(55, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(155, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(255, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(355, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(655, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(755, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(1055, 485, 'spike').setScale(0.8).refreshBody();
+    this.spikes.create(1155, 485, 'spike').setScale(0.8).refreshBody();
     this.physics.add.collider(player, this.spikes, hitBySpike, null, this);
     this.enemies.children.iterate(e => { if (e && e.play) e.play('walkRightEnemy'); });
-
-
-
     //kellon funktio
     // hae aiempi aika
     this.totalTime = this.registry.get('totalTime') || 0;
@@ -1628,7 +1632,7 @@ class Level3 extends Phaser.Scene {
         this.physics.add.overlap(player, ovi, level4Transition, null, this);
         this.physics.add.overlap(player, solid_snake_door, level1throw, null, this);
         this.cannons = [
-            this.physics.add.image(640, 265, 'cannon'),
+            this.physics.add.image(620, 300, 'cannon'),
             this.physics.add.image(620, 500, 'cannon'),
             this.physics.add.image(620, 700, 'cannon'),
         ];
@@ -2065,6 +2069,7 @@ class Level4 extends Phaser.Scene {
         this.physics.add.collider(player, bullets, hitPlayer, null, this);
         // cannon_up
         this.cannons_up = [
+            this.physics.add.image(600, 1950, 'cannon_up'),
             this.physics.add.image(650, 1950, 'cannon_up'),
             this.physics.add.image(710, 1950, 'cannon_up'),
         ];
@@ -2083,7 +2088,6 @@ class Level4 extends Phaser.Scene {
           this.cannons_up2 = [
             this.physics.add.image(1700, 1950, 'cannon_up'),
             this.physics.add.image(1950, 1950, 'cannon_up'),
-            this.physics.add.image(1390, 1950, 'cannon_up'),
         ];
         this.cannons_up2.forEach(c => { c.setImmovable(true); c.body.allowGravity = false; });
         cannon_up_bullets = this.physics.add.group({
