2c411b6207e48990a157bec6d7433241b3266b0d
Santtu-Siren-School <santtu.siren@edu.hyria.fi>
2026-01-13 13:15:02 +0200

    tutorial movment fix

--- game.js
+++ game.js
@@ -518,47 +518,80 @@ class Tutorial extends Phaser.Scene {
         }
           backgroundsound.play();
        footsteps.pause();  
-        if (cursors.up.isDown && player.body.touching.down) {
-            jumping = 1;
-            player.setVelocityY(-300);
-            player.anims.play("jump");
-            jump.play();
+        if (knockback==1) {
+            return;
         }
-
-        if (jumping === 1) {
-            player.anims.play("jump", true);
-            player.setVelocityX(0);
-            if (player.body.touching.down) {
-                jumping = 0;
+        else if (dialogueActive) {
                 player.setVelocityX(0);
-                player.anims.play('turn');
-            }
+                player.setVelocityY(0);
+                player.anims.play('turn', true);
+                return;
         }
-        if (cursors.left.isDown) {
-            if (player.body.touching.down) {
-                footsteps.play(); 
+        else {
+            if (cursors.up.isDown && player.body.touching.down) {
+                jumping = 1;
+                player.setVelocityY(-300);
+                player.anims.play("jump");
+                jump.play();
             }
-            player.setVelocityX(160);
-            player.anims.play('left', true);
-            facingRight = false;
-        } 
-        else if (cursors.right.isDown) {
-            if (player.body.touching.down) {
-                footsteps.play(); 
+            if (jumping === 1) {
+                player.anims.play("jump", true);
+                player.setVelocityX(0);
+                if (player.body.touching.down) {
+                    jumping = 0;
+                    player.setVelocityX(0);
+                    player.anims.play('turn');
+                }
             }
-            player.setVelocityX(160);
-            player.anims.play('right', true);
-            facingRight = true;
-        }
-        else if (cursors.down.isDown) {
-            player.setVelocityY(300);
-            player.anims.play('jump');
-        }  
-    
+            if (cursors.left.isDown) {
+                if (player.body.touching.down) {
+                    footsteps.play(); 
+                    player.setVelocityX(-160);
+                    player.anims.play('left', true);
+                    facingRight = false;
+                }
+                else {
+                    player.setVelocityX(-160);
+                    player.anims.play('left', true);
+                    facingRight = false;
+                    footsteps.pause();
+                }
+            } 
+            else if (cursors.right.isDown) {
+                if (player.body.touching.down) {
+                    footsteps.play(); 
+                    player.setVelocityX(160);
+                    player.anims.play('right', true);
+                    facingRight = true;
+                }
+                else {
+                    player.setVelocityX(160);
+                    player.anims.play('right', true);
+                    facingRight = true;
+                    footsteps.pause();
+                }
+            }
+            else if (cursors.down.isDown) {
+                player.setVelocityY(300);
+                player.anims.play('jump');
+            }  
             else {
-            player.setVelocityX(0)
-            player.anims.play('turn');
+                if (player.windActive) {
+                    const windAcceleration = 10;
+                    const maxWindSpeed = 200;
+                    if (player.body.velocity.x < maxWindSpeed) {
+                        player.setVelocityX(player.body.velocity.x + windAcceleration);
+                    }
+                }
+                else {
+                    player.setVelocityX(0)
+                    player.anims.play('turn');
+                }
             }
+            if (cursors.down.isDown) {
+                player.setVelocityY(300);
+                player.anims.play('jump');
+            }}
         //märitelään pelaajaan liityvää liikumista ja animaation pelausta lopuu
         //????
         if (Phaser.Input.Keyboard.JustDown(shoot)) {
@@ -2324,7 +2357,7 @@ class Level4 extends Phaser.Scene {
         }
         footsteps.pause();  
         backgroundsound.play();
-                if (knockback==1) {
+        if (knockback==1) {
             return;
         }
         else if (dialogueActive) {
