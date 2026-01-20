9a1e8cf42f384deb41c0ac2dc6c51cf80c354e7a
Niilohyria <niilo.mustonen@edu.hyria.fi>
2026-01-20 13:13:51 +0200

    .

--- game.js
+++ game.js
@@ -3904,7 +3904,7 @@ class end5 extends Phaser.Scene {
                                                     let end1img5 = this.add.image(500,450,'end1_5'); 
                                                     setTimeout(() => {
                                                         end1img5.destroy();
-                                                        let end1img6 = this.add.image(550,480,'end1_6');
+                                                        let end1img6 = this.add.image(550,480,'end1_6');try_again.play();
                                                             setTimeout(() => {end1img6.destroy();this.scene.start('credit_scene')}, 7000);
                                                             }, cutscene_1);
                                                         }, cutscene_1);
